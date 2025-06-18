8e5f80a329d3a0f8ab2495ef344ec3434017212f
eyad <eyadkhg1@gmail.com>
2025-06-18 22:16:10 +0300

    pic

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -56,7 +56,7 @@ const menuItems = [{
   description: 'Warm chocolate cake with a molten chocolate center, served with vanilla ice cream',
   price: 8.99,
   category: 'desserts',
-  image: 'https://images.unsplash.com/photo-1606313564200-e75d8e3d0e40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+  image: 'https://images.unsplash.com/photo-1542826438-c21a4267de82?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 }, {
   id: 7,
   name: 'Craft Beer Selection',
